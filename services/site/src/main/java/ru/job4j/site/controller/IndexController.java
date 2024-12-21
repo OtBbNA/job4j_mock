207eecab3fa2d4258b027a03807aead69052449f
OtBbNA <tosha.kostylev.97@mail.ru>
2024-12-21 20:57:16 +0300

    RestTemplate [#458496]

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,11 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.site.dto.CategoryDTO;
+import ru.job4j.site.dto.InterviewDTO;
 import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
@@ -31,7 +36,20 @@ public class IndexController {
                 "Главная", "/"
         );
         try {
-            model.addAttribute("categories", categoriesService.getMostPopular());
+            var categories = categoriesService.getMostPopular();
+            model.addAttribute("categories", categories);
+            Map<Integer, Integer> newByCategories = new HashMap<>();
+            for (CategoryDTO category: categories) {
+                var interviews = interviewsService.getByType(category.getId());
+                int i = 0;
+                for (InterviewDTO interview : interviews) {
+                    if (LocalDateTime.parse(interview.getCreateDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")).isAfter(LocalDateTime.now().minusDays(7))) {
+                        i++;
+                    }
+                }
+                newByCategories.put(category.getId(), i);
+            }
+            model.addAttribute("new_categories", newByCategories);
             var token = getToken(req);
             if (token != null) {
                 var userInfo = authService.userInfo(token);
